07bd5dd9959308bd7a9f14fcbcdc16d9d72a3d7d
Neel <neel642007@gmail.com>
2025-08-27 10:31:42 +0530

    Created base for convex hull

--- Tutorial-3/p2.cpp
+++ Tutorial-3/p2.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
+vector<vector<int>> convexHull(vector<vector<int>> points,int start,int end){
+    
+}
+
 int main(){
-    //What!?????
+    int n;
+    int temp;
+
+    cout<<"Enter count of the points:";
+    cin>>n;
+    vector<vector<int>> points(n,vector<int>(2,0));
+    
+    for(int i=0;i<n;i++){
+        cin>>points[i][0]>>points[i][1];
+    }
+
+    vector<vector<int>> hull;
+    hull=convexHull(points,0,n-1);
     return 0;
 }
